04fc02230005b285b93ea235766e558718306d42
AlexandruMihet <48906809+AlexandruMihet@users.noreply.github.com>
2021-02-23 18:23:56 +0200

    Add files via upload
    Second commit

---
+++ step_recognition.c
@@ -0,0 +1,7 @@
+#include <MKL25Z4.h>
+#include "step_recognition.h"
+
+
+int calculate_noise(int x){
+	return x++;
+}
---
+++ step_recognition.h
@@ -0,0 +1,7 @@
+#ifndef _STEP_RECOGNITION_H
+#define _STEP_RECOGNITION_H
+
+//Step recognition functions
+int calculate_noise(int x);
+
+#endif
